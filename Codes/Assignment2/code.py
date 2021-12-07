96ed983da619dcac67b34f15380ad03ebbfe9c9d
Sayantan <sbnimo75@gmail.com>
2021-12-07 22:48:51 +0530

    4.1 implemeneted

--- Codes/Assignment2/code.py
+++ Codes/Assignment2/code.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('cluster.csv')
-print(len(data))
-data = data.iloc[-7194:]
-
 
 grouped  = data.groupby('Vertex 1')['Vertex 2'].apply(list)
 index = np.array(grouped.index)
@@ -16,8 +13,8 @@ data['Pairs'] = data[['Vertex 1','Vertex 2']].apply(tuple,axis=1)
 phi = dict(zip(data['Pairs'],data['Amount']))
 
 
-k = 6
-kt = 3
+k = 32
+kt = 2
 
 for v in data['Vertex 1']:
     G[v].sort(key=lambda x: phi[(v,x)])
@@ -29,27 +26,14 @@ for v in data['Vertex 1']:
 V = list(data['Vertex 1'])+list(data['Vertex 2'])
 V_s = [{e} for e in V]
 C = dict(zip(V,V_s))
-print(len(C))
+print('Intial Number of Clusters:',len(C))
 count = 0
 for u in V:
     for v in G.get(u,set()):
         if(v not in C[u] and len(G[u] & G.get(v,set()))>=kt and u in G.get(v,set())):
             C[u] = C[u] | C.get(v,set())
             C[v] = C[u]
-    #print(u,C.get(v,set()))
 
-cluster = list(C.values())
-print(cluster)
+cluster = set(frozenset(s) for s in C.values())
 
-
-# v1 = np.array(data['Vertex 1'])
-# v2 = np.array(data['Vertex 2'])
-
-
-# for i in range(len(data)):
-#     phi[(data.iloc[i,0],data.iloc[i,1])] = data.iloc[i,2]
-#     if data.iloc[i,0] not in G.keys():
-#         G[data.iloc[i,0]]=  set()
-#     G[data.iloc[i,0]].add(data.iloc[i,1])
-
-print('DONE')
+print('Final Number of Clusters:',len(cluster))
